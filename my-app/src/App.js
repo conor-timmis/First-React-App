483e9c475f44b554a1145bb56e4bdf2992dd81cd
Conor Timmis <mctto@protonmail.com>
2024-08-05 19:53:46 +0000

    Create FunctionalGreetingWithProps

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
+import FunctionalGreetingWithProps from './components/FunctionalGreeting';
 
 function App() {
   return (
     <div className="App">
       <FunctionalGreeting />
-      <FunctionalGreeting />
-      <FunctionalGreeting />
-      <FunctionalGreeting />
-      <FunctionalGreeting />
+      <FunctionalGreetingWithProps />
       </div>
   );
 }
