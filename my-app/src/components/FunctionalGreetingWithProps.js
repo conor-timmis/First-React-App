659c9ab0b6fd1cb3bfc37d36b70f1e5582951e83
Conor Timmis <mctto@protonmail.com>
2024-08-05 20:01:37 +0000

    Add props to GreetingWithProps

--- my-app/src/components/FunctionalGreetingWithProps.js
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -4,6 +4,8 @@ import React from "react";
 //     return <h1>Hello from React!</h1>
 // }
 
-const FunctionalGreetingWithProps = () => <h1>Hello from React!</h1>;
-
+const FunctionalGreetingWithProps = (props) => {
+    console.log(props);
+    return <h1>Hello, {props.name} {props.age} {props.greeting}</h1>;
+}
 export default FunctionalGreetingWithProps
